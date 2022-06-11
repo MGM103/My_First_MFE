c741976cd437dd2a10b18fe2b539a8aac7fc7d2b
Marcus Marinelli <marcus.marinelli3@gmail.com>
2022-06-11 19:57:06 +1000

    Completed part 3

--- my_first_mfe/src/App.js
+++ my_first_mfe/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import Counter from './components/Counter';
 import './App.css';
 
@@ -6,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>My First MFE</h1>
         <Counter />
       </header>
